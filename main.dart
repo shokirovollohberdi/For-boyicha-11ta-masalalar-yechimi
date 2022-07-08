798c5709c1f7481d0f461b210f18c86a473b6d02
Ollohberdi <85603736+shokirovollohberdi@users.noreply.github.com>
2022-07-08 05:14:39 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,107 @@
+import 'dart:math';
+void main() {
+/*
+1-fork
+   int k = 5;
+  int n = 3;
+  for(int i=1;i<=n;i++){
+    print(k);
+  }
+*/
+
+  /* 2-Fork  
+   int a = 10;
+   int b = 15;
+   int sum = 0;
+   for (int i = a; i <= b; i++) {
+   print(i);
+   if (!(i == a || i == b)) {
+     sum++;
+    }
+   }
+  print("$a va $b orasida $sum ta son bor");
+*/
+ 
+  /* 3-masala
+  int a = 10;
+  int b=20;
+  int sum = 0;
+  for(int i=b;i>=a;i--){
+     print(i);
+   if (!(i == a || i == b)) {
+     sum++;
+    }
+  }
+  print("$a va $b orasida $sum ta son bor");// 10 va 20 inobatga olinmagan
+*/
+
+  /* 4- Masla
+  int narx = 10000;
+  for(int i=1;i<=10;i++){
+    print("$i kg shakar narxi ${i*narx}");
+  }
+*/
+
+  /* 5- masala
+  
+  int narx = 10000;
+  for(int i=1;i<=10;i++){
+    double a = i/10;
+    print("$a kg shakar narxi ${a*i}");
+  }
+  */
+
+  /* 7- masala  
+  int a=5;
+  int b=10;
+  int sum = 0;
+  for(int i=a;i<=b;i++){
+    sum+=i;
+  }
+  print(sum);
+  */
+
+  /*   8-masala
+
+  int a = 3;
+  int b = 5;
+  int sum=1;
+  for(int i=a;i<=b;i++){
+    sum*=i;
+  }
+  print(sum);
+*/
+
+
+  /* 9-masala 
+ int a=2;
+  int b=5;
+  double sum=0;
+
+  for(int i=a;i<=b;i++){
+    sum+= pow(i.toDouble(),2.toDouble());
+  }
+  print(sum);
+*/
+ /* 10-masala
+    int n = 100;
+  double s = 0.0;
+  for(int i=1;i<=n;i++){
+    s+=1/i;
+  }
+  print(s);
+*/
+
+  
+  /*11-masala
+  int n =5;
+  double s = 0.0;
+  for(int i=1;i<=n;i++){
+    s+= pow(n+i,2);
+  }
+  print(s);
+  */
+
+  
+  
+}
